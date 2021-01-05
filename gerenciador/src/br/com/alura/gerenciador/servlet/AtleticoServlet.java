fc1a6dd5d7570e83ee269b71c39aa9fead196a28
ricardowoj <ricardowoj@hotmail.com>
2021-01-04 21:12:35 -0300

    Criado Servlet

---
+++ gerenciador/src/br/com/alura/gerenciador/servlet/AtleticoServlet.java
@@ -0,0 +1,22 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/atletico")
+public class AtleticoServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<p> Aqui é GALO! </p>");
+		
+		System.out.println("Servlet foi chamado...");
+	}
+}
