7a507c2760dc3c25546d857eed9a806d293b77ad
ricardowoj <ricardowoj@hotmail.com>
2021-01-05 19:10:44 -0300

    metodos get e post

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovoJogador.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovoJogador.java
@@ -9,19 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class NovoServlet
- */
-@WebServlet("/NovoServlet")
-public class NovoServlet extends HttpServlet {
+@WebServlet("/novoJogador")
+public class NovoJogador extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Novo Servlet");
-		String atacante = request.getParameter("atacante");
+		String atacante = request.getParameter("nome");
 		PrintWriter out = response.getWriter();
-		out.println("<p> Aqui é GALO! </p>");
-		out.print("<p> Atacante: " + atacante + "</p>");
+		out.println("<p> Clube Atlético Mineiro </p>");
+		out.print("<p> Jogador: " + atacante + " cadastrado com sucesso</p>");
 	}
 
 }
